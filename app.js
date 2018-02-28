cc79284f80e588e3e560b4c3ad6e15bc60e89fe4
Piepognwong <j.tonneyck@gmail.com>
2018-02-28 16:41:26 +0100

    little change

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.set("PORT", 3000)
 app.get("/", (req, res) => {
 
 	console.log("Reached")
-	res.status(200).send("HI, I'm working")
+	res.status(200).send("HI, I'm working. And I am a change!")
 
 })
 
