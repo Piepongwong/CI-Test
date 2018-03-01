bd91ebf960e268df2173a12828c8740e6478e944
Piepognwong <j.tonneyck@gmail.com>
2018-03-01 01:37:51 +0100

    no jenkinsfile

--- test/test.js
+++ test/test.js
@@ -5,7 +5,6 @@ describe('Homepage', function() {
   describe('Status', function() {
     it('should return status code 200', function() {
      request("http://localhost:3000", function(error, response, body) {
-     	console.log(body)
       assert.equal(response.statusCode.indexOf(200), -1);     	
      })
     });
